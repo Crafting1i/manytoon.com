6dcd08321c24daf1a02921a000cd5a972fbdf504
Crafting1i <dauve228@gmail.com>
2022-06-12 12:16:06 +0300

    Delete popular.js

--- test/popular.js
+++
@@ -1,9 +0,0 @@
-const manytoon = require('../src')
-
-const parser = new manytoon.Parser()
-
-async function getPopular() {
-    const popular = await parser.getPopularUpdates()
-    console.log(popular)
-}
-getPopular()
